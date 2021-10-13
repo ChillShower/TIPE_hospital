5847b68907b6d7d6c79a11362c6c258360953c69
G0TH4MM4G1C <92450337+G0TH4MM4G1C@users.noreply.github.com>
2021-10-13 16:31:46 +0200

    on ajoute l'arbre
    reste a coder le reste

---
+++ TIPE.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+class arbre :
+    valeur = None
+    G = None
+    D = None
+
+
+    def _init_ (self , valeur , aG , aD ):
+        self.val = valeur
+        self.G = aG
+        self.D = aD
+
+    def ajouter_fils (self , a cote):
+        if cote == "G" :
+            self.G = a
+        elif cote == "D"
+            self.D = a
+
+
+Op = """liste avec temps opération croissant"""
+S ="""liste avec le temps des salles"""
+
+def remplir_arbre (Op , S):
+    ajouter_fils (S[0])
+    for i in Op :
+        while
+
+
+
+
+
