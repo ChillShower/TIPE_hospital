5e4aa46594fb51352d2e75cf9732051d266b5fba
clarapzl <92449970+clarapzl@users.noreply.github.com>
2021-10-20 17:53:04 +0200

    Add files via upload
    j'ai rien fait

--- TIPE.py
+++ TIPE.py
@@ -19,24 +19,27 @@ class arbre :
 Op = """liste avec temps opération croissant"""
 S ="""liste avec le temps des salles"""
 
-arbre = arbre(S[0])
-
-def remplir_arbre (Op , S):
-    init( S[0] )
-    for i in Op :
-        if Op[i] < S[0] :
-            arbre.ajouter_fils_g ("O"+str(i))
-            arbre.ajouter_fils_d ("nO"+str(i))
-            S[0] = S[0] - Op[i]
-        else :
-            arbre.ajouter_fils_d ("nO"+str(i))
+a = arbre(S[0])
 
 
 
 
-def remplir_arbre_rec (Op , S , arbre) :
-    if S[0] != 0 :
-        arbre.ajouter_fils_d ()
+
+
+def remplir_arbre (Op , S , a) :
+    if Op == [] :
+        return a
+    elif :
+
+    while S[0] != 0 :
+        for i in range (len (Op)) :
+            if Op[i] <= S[0] :
+                a.ajouter_fils_g ("O" + str(i))
+                a.ajouter_fils_d ("no" + str(i))
+                S[0] = S[0] - Op[i]
+            else :
+                a.ajouter_fils_d ("nO" + str(i))
+
 
 
 
