1a386d8627f1d79aa2478c7d6d9f75554be9b681
G0TH4MM4G1C <92450337+G0TH4MM4G1C@users.noreply.github.com>
2021-11-10 17:41:09 +0100

    Add files via upload

---
+++ TIPE pyzo 1011.py
@@ -0,0 +1,42 @@
+def mot(n):
+    L=[]
+    if n==1:
+        return ["0","1"]
+    else:
+        m=mot(n-1)
+        for i in range(len(m)):
+            L.append("1"+m[i])
+            L.append("0"+m[i])
+        return L
+        
+def salle2(L):
+    M=[]
+    for i in range(len(2**n)):
+        for j in range(len(n)):
+            if L[j]==1:
+                L[j]=0
+                M=[L[j]]
+            else:
+                L[j]=0 or L[j]=1
+                M=[L[j]]
+            
+            
+            
+        
+        
+
+"""def mot_bis(p,n):
+    pass
+    L=[]
+    for p in range(n):
+        m=mot(n-p)
+        for i in range (len(m)):"""
+        
+
+
+
+
+
+print(mot(10))
+
+
